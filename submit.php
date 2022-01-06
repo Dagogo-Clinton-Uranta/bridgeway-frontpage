441dc97b6b8d6ef3b2de37b2e53e8a3d6510d4b6
Dagogo Clinton Uranta <dagogouranta@gmail.com>
2022-01-06 12:58:14 +0100

    changed uploads to / in submit.php file

--- submit.php
+++ submit.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
             if(!empty($_FILES["attachment"]["name"])){
                 
                 // File path config
-                $targetDir = "uploads/";
+                $targetDir = "/";
                 $fileName = basename($_FILES["attachment"]["name"]);
                 $targetFilePath = $targetDir . $fileName;
                 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
